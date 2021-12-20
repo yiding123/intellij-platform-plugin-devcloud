153fa31cf3a9bed24d5daf3b0e446b90c5c6c99c
GitHub Action <action@github.com>
2021-12-20 06:34:30 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/yiding123/intellijplatformplugindevcloud/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.yiding123.intellijplatformplugindevcloud.services
+
+import com.github.yiding123.intellijplatformplugindevcloud.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
